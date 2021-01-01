0434ae7cdb4192d1733f9d70c3b6690ab4e203fc
M-Usamah <mohammad.usamah127@hotmail.com>
2021-01-01 21:25:41 +0500

    stage 2 done

--- Logic.py
+++ Logic.py
@@ -1,11 +1,21 @@
 import pytube
+import os
 
-dowlink = input("Enter a vidoe link to download: ")
-try:
-    yt = pytube.YouTube(dowlink)
-    showName = yt.title
-    print(showName)
-    Stream = yt.streams.filter(progressive=True,file_extension='mp4').last()
-    Stream.download()
-except Exception as link:
-    print('Error...\nEnter a correct Link')
+def video():
+    dowlink = input("Enter a link to download vidoe: ")
+    try:
+        yt = pytube.YouTube(dowlink)
+        showName = yt.title
+        print(showName)
+        Stream = yt.streams.filter(progressive=True,file_extension='mp4').last()
+        Stream.download()
+    except Exception as link:
+        print('Error...\nEnter a correct Link')
+
+def Playlist():
+    dowlink = input("Enter a link to download full playlist: ")
+    yt = pytube.Playlist(dowlink)
+    print(yt.title)
+    for videos in yt.videos:
+        videos.streams.filter(progressive=True,file_extension='mp4').last().download()
+
