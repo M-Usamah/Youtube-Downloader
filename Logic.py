50c6e6968c2ba7a5fcceaa3ffe9a0bc4a637aec4
M-Usamah <mohammad.usamah127@hotmail.com>
2021-08-07 17:03:18 +0500

    done

--- Logic.py
+++ Logic.py
@@ -1,9 +1,14 @@
 import pytube
 from tkinter import messagebox
 import os
-
-def video(dowlink,d = os.chdir("/home/evildevil127/Downloads")):
-    
+import earthpy as et
+Home = et.io.HOME
+saveToFile = Home + '\Downloads'
+saveFiles = os.path.join(saveToFile)
+saveFile = os.chdir(saveFiles)
+saveFile
+def video(dowlink,dowFile = saveFile):
+    dowFile
     try:
         yt = pytube.YouTube(dowlink)
         Stream = yt.streams.filter(progressive=True,file_extension='mp4').last()
@@ -15,7 +20,8 @@ def video(dowlink,d = os.chdir("/home/evildevil127/Downloads")):
     except Exception as link:
         messagebox.showerror("error",'Enter a correct Link')
 
-def Playlist(dowlink, d = os.chdir("/home/evildevil127/Downloads")):
+def Playlist(dowlink, downfile = saveFile):
+    downfile
     try:
         yt = pytube.Playlist(dowlink)
         for videos in yt.videos:
@@ -28,7 +34,4 @@ def Playlist(dowlink, d = os.chdir("/home/evildevil127/Downloads")):
         messagebox.showerror("error",'Enter a correct Link')
 
 if __name__ == "__main__":
-    # os.chdir("/home/evildevil127/Downloads")
-    print(os.getcwd())
     video("https://youtu.be/1XEZBKvo5Uc")
-
